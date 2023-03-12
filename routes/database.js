583fe53efe7da184039881928841b3966bae8871
Batu Alp <68554767+Batu-Alp@users.noreply.github.com>
2023-03-12 21:55:47 +0300

    Update database.js

--- routes/database.js
+++ routes/database.js
@@ -4,8 +4,8 @@ const connection = mysql.createConnection({
     host: "localhost",
     port : 3306,
     user: "root",
-    password: "Mydata13*batu",
-    database: "login_register"
+    password: "******",
+    database: "******"
 });
 
 
